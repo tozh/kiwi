db973a3ee3e4213b92e6756e4001c7e99c8c2d44
zhaotong0312 <zhaotong0312@outlook.com>
2018-09-10 02:01:50 -0700

    close chan

--- main/main.go
+++ main/main.go
@@ -1,40 +1,40 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "redigo/src/server"
 
 func main() {
-	ch := make(chan int, 3)
-	go haha1(ch)
-	go haha2(ch)
-	go haha3(ch)
-	//ch<-1
-	//ch<-1
-	//ch<-1
-	close(ch)
-	time.Sleep(time.Second)
+	//ch := make(chan int, 3)
+	//go haha1(ch)
+	//go haha2(ch)
+	//go haha3(ch)
+	////ch<-1
+	////ch<-1
+	////ch<-1
+	//close(ch)
+	//time.Sleep(time.Second)
+	s := server.CreateServer()
+	server.StartServer(s)
+	server.HandleSignal(s)
 }
 
-func haha1(ch chan int) {
-	var num int
-	select {
-		case num=<-ch:
-			fmt.Println(1, num)
-	}
-}
-func haha2(ch chan int) {
-	var num int
-	select {
-		case num=<-ch:
-			fmt.Println(2, num)
-	}
-}
-func haha3(ch chan int) {
-	var num int
-	select {
-		case num=<-ch:
-			fmt.Println(3, num)
-	}
-}
+//func haha1(ch chan int) {
+//	var num int
+//	select {
+//		case num=<-ch:
+//			fmt.Println(1, num)
+//	}
+//}
+//func haha2(ch chan int) {
+//	var num int
+//	select {
+//		case num=<-ch:
+//			fmt.Println(2, num)
+//	}
+//}
+//func haha3(ch chan int) {
+//	var num int
+//	select {
+//		case num=<-ch:
+//			fmt.Println(3, num)
+//	}
+//}
